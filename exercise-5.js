dcb6537a93c65f76171f981eccf3a8586857f5b6
yasirmaulana <yasir.maulana@gmail.com>
2017-12-19 23:48:57 +0700

    menambahkan tugas 5

---
+++ exercise-5.js
@@ -0,0 +1,9 @@
+console.log('LOOPING PERTAMA');
+for(var angka = 1; angka <= 20; angka++) {
+  console.log(angka +' - I love coding');
+}
+
+console.log('LOOPING KEDUA');
+for(var angka2 = 20; angka2 >= 1; angka2--) {
+  console.log(angka2 +' - I will become fullstack developer');
+}
